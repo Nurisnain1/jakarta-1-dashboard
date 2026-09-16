431812d2bb99cf20adc9bc26052179da5bb66f95
Nurisnain1 <isnamuntaco@gmail.com>
2026-09-17 03:58:43 +0700

    Bump LOB device year-switch fix

--- app/page.tsx
+++ app/page.tsx
@@ -9,5 +9,5 @@ import Jakarta1YoYDevice from "@/components/jakarta1-yoy-device";
 import Jakarta1YoYShortcut from "@/components/jakarta1-yoy-shortcut";
 
 export default function Home(){
-  return <><Jakarta1DashboardV2/><Jakarta1DailyFocus/><Jakarta1TargetFocus/><Jakarta1WeeklyM238/><Jakarta1Soh/><Jakarta1SohUpdateTrigger/><Jakarta1IncentiveAudit/><Jakarta1YoYDevice/><Jakarta1YoYShortcut/><script src="/jakarta1-enhancer.js?v=20260917-1" defer /><script src="/jakarta1-area-fix.js?v=20260917-0345" defer /><script src="/jakarta1-ytd-fix.js?v=20260917-1" defer /><script src="/jakarta1-lob-device.js?v=20260917-0350" defer /><script src="/jakarta1-responsive.js" defer /></>;
+  return <><Jakarta1DashboardV2/><Jakarta1DailyFocus/><Jakarta1TargetFocus/><Jakarta1WeeklyM238/><Jakarta1Soh/><Jakarta1SohUpdateTrigger/><Jakarta1IncentiveAudit/><Jakarta1YoYDevice/><Jakarta1YoYShortcut/><script src="/jakarta1-enhancer.js?v=20260917-1" defer /><script src="/jakarta1-area-fix.js?v=20260917-0345" defer /><script src="/jakarta1-ytd-fix.js?v=20260917-1" defer /><script src="/jakarta1-lob-device.js?v=20260917-0400" defer /><script src="/jakarta1-responsive.js" defer /></>;
 }
