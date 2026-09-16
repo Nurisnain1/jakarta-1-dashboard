9bb691130500cca701d89df91716a2bea7a79629
Nurisnain1 <isnamuntaco@gmail.com>
2026-09-16 18:38:46 +0700

    Add Target Fokus menu Jakarta 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Jakarta1DashboardV2 from "@/components/jakarta1-dashboard-v2";
 import Jakarta1DailyFocus from "@/components/jakarta1-daily-focus";
+import Jakarta1TargetFocus from "@/components/jakarta1-target-focus";
 import Jakarta1WeeklyM238 from "@/components/jakarta1-weekly-m238";
 import Jakarta1Soh from "@/components/jakarta1-soh-v2";
 import Jakarta1SohUpdateTrigger from "@/components/jakarta1-soh-update-trigger";
@@ -8,5 +9,5 @@ import Jakarta1YoYDevice from "@/components/jakarta1-yoy-device";
 import Jakarta1YoYShortcut from "@/components/jakarta1-yoy-shortcut";
 
 export default function Home(){
-  return <><Jakarta1DashboardV2/><Jakarta1DailyFocus/><Jakarta1WeeklyM238/><Jakarta1Soh/><Jakarta1SohUpdateTrigger/><Jakarta1IncentiveAudit/><Jakarta1YoYDevice/><Jakarta1YoYShortcut/><script src="/jakarta1-enhancer.js" defer /></>;
+  return <><Jakarta1DashboardV2/><Jakarta1DailyFocus/><Jakarta1TargetFocus/><Jakarta1WeeklyM238/><Jakarta1Soh/><Jakarta1SohUpdateTrigger/><Jakarta1IncentiveAudit/><Jakarta1YoYDevice/><Jakarta1YoYShortcut/><script src="/jakarta1-enhancer.js" defer /></>;
 }
