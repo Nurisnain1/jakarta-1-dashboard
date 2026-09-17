1ff9755d26cb9291710a0457f2fbbd8ad4a391a2
Nurisnain1 <isnamuntaco@gmail.com>
2026-09-17 22:42:09 +0700

    Apply Staff Performance column revision

--- app/page.tsx
+++ app/page.tsx
@@ -11,5 +11,5 @@ import Jakarta1LobDevicePerformance from "@/components/jakarta1-lob-device-perfo
 import Jakarta1YoYStoreFilter from "@/components/jakarta1-yoy-store-filter";
 
 export default function Home(){
-  return <><Jakarta1DashboardV2/><Jakarta1LobDevicePerformance/><Jakarta1YoYStoreFilter/><Jakarta1DailyFocus/><Jakarta1TargetFocus/><Jakarta1WeeklyM238/><Jakarta1Soh/><Jakarta1SohUpdateTrigger/><Jakarta1IncentiveAudit/><Jakarta1YoYDevice/><Jakarta1YoYShortcut/><script src="/jakarta1-enhancer.js?v=20260917-1" defer /><script src="/jakarta1-area-fix.js?v=20260917-0345" defer /><script src="/jakarta1-ytd-fix.js?v=20260917-1" defer /><script src="/jakarta1-responsive.js" defer /></>;
+  return <><Jakarta1DashboardV2/><Jakarta1LobDevicePerformance/><Jakarta1YoYStoreFilter/><Jakarta1DailyFocus/><Jakarta1TargetFocus/><Jakarta1WeeklyM238/><Jakarta1Soh/><Jakarta1SohUpdateTrigger/><Jakarta1IncentiveAudit/><Jakarta1YoYDevice/><Jakarta1YoYShortcut/><script src="/jakarta1-enhancer.js?v=20260917-1" defer /><script src="/jakarta1-staff-performance-fix.js?v=20260917-2" defer /><script src="/jakarta1-area-fix.js?v=20260917-0345" defer /><script src="/jakarta1-ytd-fix.js?v=20260917-1" defer /><script src="/jakarta1-responsive.js" defer /></>;
 }
