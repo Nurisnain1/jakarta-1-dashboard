4b92aabeb24f117ae353bc53ee19e95838707145
Nurisnain1 <isnamuntaco@gmail.com>
2026-09-18 13:16:12 +0700

    Switch report capture to html-to-image

--- components/jakarta1-report-deps.tsx
+++ components/jakarta1-report-deps.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { useEffect } from "react";
-import html2canvas from "html2canvas";
+import * as htmlToImage from "html-to-image";
 import * as XLSX from "xlsx";
 
 export default function Jakarta1ReportDeps() {
   useEffect(() => {
-    (window as any).html2canvas = html2canvas;
+    (window as any).htmlToImage = htmlToImage;
     (window as any).XLSX = XLSX;
     window.dispatchEvent(new Event("jakarta1-report-deps-ready"));
   }, []);
