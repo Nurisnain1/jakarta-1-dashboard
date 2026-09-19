81e4505897c5429814c6dda1a20beefd20478b87
Nurisnain1 <isnamuntaco@gmail.com>
2026-09-19 15:41:25 +0700

    Remove separate Product LOB Aktif menu mount

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Jakarta1DashboardV2 from "@/components/jakarta1-dashboard-v2";
 import Jakarta1DailyFocus from "@/components/jakarta1-daily-focus";
 import Jakarta1TargetFocus from "@/components/jakarta1-target-focus";
-import Jakarta1ProductLobActive from "@/components/jakarta1-product-lob-active";
 import Jakarta1TargetFocus3PP from "@/components/jakarta1-target-focus-3pp";
 import Jakarta1TargetFocusVAS from "@/components/jakarta1-target-focus-vas";
 import Jakarta1WeeklyM238 from "@/components/jakarta1-weekly-m238";
@@ -14,5 +13,5 @@ import Jakarta1PromoSource from "@/components/jakarta1-promo-source";
 import Jakarta1ReportDeps from "@/components/jakarta1-report-deps";
 
 export default function Home(){
-  return <><Jakarta1ReportDeps/><Jakarta1DashboardV2/><Jakarta1LobDevicePerformance/><Jakarta1YoYStoreFilter/><Jakarta1DailyFocus/><Jakarta1TargetFocus/><Jakarta1ProductLobActive/><Jakarta1TargetFocus3PP/><Jakarta1TargetFocusVAS/><Jakarta1WeeklyM238/><Jakarta1Soh/><Jakarta1SohUpdateTrigger/><Jakarta1IncentiveAudit/><Jakarta1PromoSource/><script src="/jakarta1-enhancer.js?v=20260918-2" defer /><script src="/jakarta1-reason-labels.js?v=20260918-1" defer /><script src="/jakarta1-reason-week.js?v=20260918-1" defer /><script src="/jakarta1-area-fix.js?v=20260917-0345" defer /><script src="/jakarta1-ytd-fix.js?v=20260917-1" defer /><script src="/jakarta1-responsive.js?v=20260918-2" defer /><script src="/jakarta1-report-tools.js?v=20260918-8" defer /></>;
+  return <><Jakarta1ReportDeps/><Jakarta1DashboardV2/><Jakarta1LobDevicePerformance/><Jakarta1YoYStoreFilter/><Jakarta1DailyFocus/><Jakarta1TargetFocus/><Jakarta1TargetFocus3PP/><Jakarta1TargetFocusVAS/><Jakarta1WeeklyM238/><Jakarta1Soh/><Jakarta1SohUpdateTrigger/><Jakarta1IncentiveAudit/><Jakarta1PromoSource/><script src="/jakarta1-enhancer.js?v=20260918-2" defer /><script src="/jakarta1-reason-labels.js?v=20260918-1" defer /><script src="/jakarta1-reason-week.js?v=20260918-1" defer /><script src="/jakarta1-area-fix.js?v=20260917-0345" defer /><script src="/jakarta1-ytd-fix.js?v=20260917-1" defer /><script src="/jakarta1-responsive.js?v=20260918-2" defer /><script src="/jakarta1-report-tools.js?v=20260918-8" defer /></>;
 }
